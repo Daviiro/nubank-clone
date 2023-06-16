18a3ade2eb69c821d108ff77d5c1c8f6881a53b3
David Jr <david.junior@unesp.br>
2023-06-16 17:19:12 -0300

    feat: account balance

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header";
+import Balance from "../../components/Balance";
 
 export default function Home() {
 	return (
 		<View style={styles.container}>
 			<Header user="David" />
-			<Text>Home da Aplicação!</Text>
+			<Balance balance={800.25} />
 			<StatusBar style="auto" />
 		</View>
 	);
