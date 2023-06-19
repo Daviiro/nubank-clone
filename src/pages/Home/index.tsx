133e3b811518e4041367293bcddbc74ba2ea1320
David Jr <david.junior@unesp.br>
2023-06-19 17:31:49 -0300

    feat: my cards section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "../../components/Header";
 import Balance from "../../components/Balance";
 import FeaturesTab from "../../components/FeaturesTab";
-import CreditCard from "../../components/creditCard";
+import MyCards from "../../components/myCards";
 
 export default function Home() {
 	return (
@@ -12,7 +12,7 @@ export default function Home() {
 			<Header user="David" />
 			<Balance balance={800.25} />
 			<FeaturesTab />
-			<CreditCard />
+			<MyCards />
 		</View>
 	);
 }
