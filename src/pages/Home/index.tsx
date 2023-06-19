cb9cad48b9dd2097a8be3750c8c8af8dd3360aec
David Jr <david.junior@unesp.br>
2023-06-19 18:03:36 -0300

    feat: news section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import Header from "../../components/Header";
 import Balance from "../../components/Balance";
 import FeaturesTab from "../../components/FeaturesTab";
 import MyCards from "../../components/myCards";
+import News from "../../components/News";
 
 export default function Home() {
 	return (
 		<View style={styles.container}>
 			<StatusBar style="auto" />
-			<Header user="David" />
-			<Balance balance={800.25} />
-			<FeaturesTab />
-			<MyCards />
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Header user="David" />
+				<Balance balance={800.25} />
+				<FeaturesTab />
+				<MyCards />
+				<News />
+			</ScrollView>
 		</View>
 	);
 }
