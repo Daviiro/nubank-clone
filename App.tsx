49f3a0dfd40e523a8f148e8ba569ee36e339b281
David Jr <david.junior@unesp.br>
2023-06-20 12:57:58 -0300

    feat: navigation setup 2

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
 import Home from "./src/pages/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
-	return <Home />;
+	const Stack = createNativeStackNavigator();
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				<Stack.Screen name="Home" component={Home} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
