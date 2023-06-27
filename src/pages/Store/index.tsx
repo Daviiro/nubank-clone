59a880e3c97b98b5e7e4c5cf3291ddcae3668e64
David Jr <david.junior@unesp.br>
2023-06-27 11:52:52 -0300

    fix: navigation through the tabbar

---
+++ src/pages/Store/index.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Header from "../../components/Header";
+
+const styles = StyleSheet.create({
+	container: {},
+});
+
+const Store: React.FC = () => {
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Header user="David" />
+			</ScrollView>
+		</View>
+	);
+};
+
+export default Store;
